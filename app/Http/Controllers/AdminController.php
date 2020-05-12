6b7ab6fe48fbc090b4b8389fc0fda2226abd443b
0length <fajarjunyor13@gmail.com>
2020-05-12 15:30:05 +0700

    hjk

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\File\File;
+use Illuminate\Support\Facades\File;
 class AdminController extends Controller
 {
     /**
